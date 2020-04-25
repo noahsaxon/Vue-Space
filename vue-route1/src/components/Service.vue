b69604519decd5da69c7ce6b190212e0f6782bb4
noahsaxon <noahsaxon9@gmail.com>
2020-04-24 22:00:39 -0400

    vue-router

---
+++ vue-route1/src/components/Service.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Service
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Service',
+    props:[]
+}
+</script>
---
+++ vue-route1/src/views/Service.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="service">
+      <service/>
+  </div>
+</template>
