80747ea3fb9c8024c23086869672a936beb3fb5f
noahsaxon <noahsaxon9@gmail.com>
2020-04-30 23:24:08 -0400

    crear usuarios y mostrar errores de autenticacion

--- firebase-auth/src/store/index.js
+++ firebase-auth/src/store/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+var firebase = require("firebase/app")
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usuario : '',
+    error: ''
   },
   mutations: {
-  },
+    setUsuario(state, payload) {
+      state.usuario = payload 
+    },
+    setError(state, payload){
+      state.error = payload
+    }
+  }, 
   actions: {
+    createUsuario({commit}, payload){
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password).then( res =>{
+        console.log(res)
+        commit(setUsuario , {email: res.user.email , uid:res.user.uid})
+      }).catch(function(error) {
+        commit('setError' , error.message)
+      });
+    }
   },
   modules: {
   }
--- firebase-auth/src/views/Registro.vue
+++ firebase-auth/src/views/Registro.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <h1>Registro de usuarios</h1>
-        <form action="">
+        <form @submit.prevent="createUsuario({email:email , password: pass1})">
             <input type="email" v-model="email">
             <input type="password"  v-model="pass1">
             <input type="password"  v-model="pass2">
             <button type="submit">Crear Usuario</button>
         </form>
+        <p>{{error}}</p>
     </div>
 </template>
 
 
 <script>
+
+import {mapActions , mapState} from 'vuex';
+
 export default {
     name:'Registro',
     data(){
@@ -20,6 +24,12 @@ export default {
             pass1:'',
             pass2:''
         }
+    },
+    methods: {
+        ...mapActions(['createUsuario'])
+    },
+    computed:{
+        ...mapState(['error'])
     }
 }
 </script>
