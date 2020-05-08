09ff502c422be8ca10458268e98c22541df75d30
noahsaxon <noahsaxon9@gmail.com>
2020-05-08 15:06:04 -0400

    more inputs

--- bootstrap-project/src/views/Form.vue
+++ bootstrap-project/src/views/Form.vue
@@ -2,11 +2,35 @@
     <div>
         <h1>Formulario</h1>
         <form action="">
-                <b-form-input type="number" v-model="numero"></b-form-input>
-                <b-form-input type="text" v-model="texto" :state="comprobarLogitud"></b-form-input>
-                <small>Minimo 3 caracteres</small>
+                <b-row>
+                    <b-col md="3"  >
+                        <b-form-input type="number" v-model="numero"></b-form-input>
+                        <b-form-input type="text" v-model="texto" :state="comprobarLogitud"></b-form-input>
+                        <small>Minimo 3 caracteres</small>
+                        <p>texto: {{texto}}</p>
+                    </b-col>
+                    <b-col md="3"> 
+                        <b-form-select v-model="selected" :options="options" size="sm" class="mt-3"></b-form-select>
+                    </b-col>
+                    <b-col md="3">
+                        <b-form-group label="Individual radios">
+                            <b-form-radio v-model="selected" name="some-radios" value="A">Option A</b-form-radio>
+                            <b-form-radio v-model="selected" name="some-radios" value="B">Option B</b-form-radio>
+                            </b-form-group>
+                    </b-col>
+                    <b-cool md="3">
+                        <b-form-checkbox-group
+                            id="checkbox-group-1"
+                            v-model="selected2"
+                            :options="options"
+                            name="flavour-1"
+                        ></b-form-checkbox-group>
+                        <div>Selected: <strong>{{ selected2 }}</strong></div>
+                    </b-cool>
+                </b-row>
+
         </form>
-        <p>texto: {{texto}}</p>
+
     </div>
 </template>
 
@@ -16,7 +40,17 @@ export default {
     data(){
         return {
             texto:'',
-            numero:''
+            numero:'',
+            selected2:[],
+            selected:[],
+            options: [
+                { value: null, text: 'Please select an option' },
+                { value: 'a', text: 'This is First option' },
+                { value: 'b', text: 'Selected Option' },
+                { value: { C: '3PO' }, text: 'This is an option with object value' },
+                { value: 'd', text: 'This one is disabled', disabled: true }
+            ],
+            check:[]
         }
     },
     computed: {
