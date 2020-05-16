c705cb287c2292f82370166e0f89bcb4a3f11b72
noahsaxon <noahsaxon9@gmail.com>
2020-05-16 16:46:29 -0400

    vuetify

--- vuetify_test/src/router/index.js
+++ vuetify_test/src/router/index.js
@@ -13,18 +13,14 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/grid',
-    name: 'Grid',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Grid.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
