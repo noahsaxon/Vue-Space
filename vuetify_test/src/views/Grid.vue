3ab1131b01fad3ff2626ac39d1553764a1280c96
noahsaxon <noahsaxon9@gmail.com>
2020-05-16 18:07:44 -0400

    grid vuetify

---
+++ vuetify_test/src/views/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="6">
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloremque delectus consectetur ad ipsa, nam quibusdam dolores, deserunt, eaque obcaecati ipsam autem libero laudantium odit perferendis voluptates numquam ut? Eum.</p>
+            </v-col >
+            <v-col cols="6" sm="6">
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloremque delectus consectetur ad ipsa, nam quibusdam dolores, deserunt, eaque obcaecati ipsam autem libero laudantium odit perferendis voluptates numquam ut? Eum.</p>
+            </v-col>
+
+        </v-row>
+
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    name:'Grid'
+}
+</script>
