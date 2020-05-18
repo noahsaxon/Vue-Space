8115ba1990d81e1f14f3fc403b960394589dd5f8
noahsaxon <noahsaxon9@gmail.com>
2020-05-17 20:23:52 -0400

    calendario

---
+++ vuetify_test2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ vuetify_test2/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-layout :wrap="true">
+      <v-flex x12 >
+        <v-card>
+          <v-date-picker v-model="fecha" full-width dark locale="es">
+
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            650 - {{fecha}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data(){
+    return {
+      fecha:''
+    }
+  }
+}
+</script>
