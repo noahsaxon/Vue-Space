0217eefaf106fce88b74cba16c526873908f9882
noahsaxon <noahsaxon9@gmail.com>
2020-05-19 17:11:44 -0400

    vuetify + axios

--- vuetify_test2/src/App.vue
+++ vuetify_test2/src/App.vue
@@ -2,21 +2,47 @@
   <v-app>
     <v-content>
       <router-view></router-view>
+
+      <v-dialog
+            v-model="loading.estado"
+            hide-overlay
+            persistent
+            width="300"
+          >
+            <v-card
+              color="danger"
+              dark
+            >
+              <v-card-text>
+                {{loading.titulo}}
+                <v-progress-linear
+                  indeterminate
+                  color="white"
+                  class="mb-0"
+                ></v-progress-linear>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+          
+
     </v-content>
   </v-app>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld';
-
+import {mapState} from 'vuex'
 export default {
   name: 'App',
 
   components: {
+
   },
 
   data: () => ({
-    //
   }),
+  computed: {
+    ...mapState(['loading'])
+  },
 };
 </script>
--- vuetify_test2/src/views/Home.vue
+++ vuetify_test2/src/views/Home.vue
@@ -3,13 +3,13 @@
     <v-layout :wrap="true">
       <v-flex x12 >
         <v-card>
-          <v-date-picker v-model="fecha" full-width dark locale="es" :min="minimo" :max="maximo">
+          <v-date-picker v-model="fecha" full-width dark locale="es" :min="minimo" :max="maximo" @change="getDolar(fecha)">
 
           </v-date-picker>
         </v-card>
         <v-card color="error" dark>
           <v-card-text class="display-1 text-center">
-            650 
+            {{valor}} 
           </v-card-text>
         </v-card>
       </v-flex>
@@ -21,16 +21,41 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
-import {axios} from "axios"; 
-
+import axios from "axios"; 
+import {mapMutations} from "vuex"
 export default {
   name: 'Home',
   data(){
     return {
-      fecha:'',
+      fecha: new Date().toISOString().substr(0, 10),
       minimo: "1984",
-      maximo: new Date().toISOString().substr(0, 10)
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null
     }
+  }, methods: {
+    ...mapMutations(['mostrarLoading' , 'ocultarLoading']),
+    async getDolar(dia){
+      let arrayFecha = dia.split(['-'])
+      dia = arrayFecha[2] + '-' + arrayFecha[1] + '-' +arrayFecha[0]
+      try {
+        this.mostrarLoading({titulo:"Accediendo a Informacion"})
+        let datos = await axios.get(`https://mindicador.cl/api/dolar/${dia}`);
+        if(datos.data.serie.length > 0 ){
+          this.valor = await datos.data.serie[0].valor;        
+        } else {
+          this.valor = "Sin Resultados"
+        }
+        
+      } catch (error) {
+        console.log(error)
+      } finally{
+        this.ocultarLoading();
+      }
+
+    }
+  },
+  created(){
+    this.getDolar(this.fecha)
   }
 }
 </script>
