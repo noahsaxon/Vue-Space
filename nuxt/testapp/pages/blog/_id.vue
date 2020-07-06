6edc44dee2e611d6425a8eeeffe8bfcfb6238ec3
noahsaxon <noahsaxon9@gmail.com>
2020-07-05 23:36:24 -0400

    vuejs + nuxtjs + bootstrap

--- nuxt/testapp/pages/blog/_id.vue
+++ nuxt/testapp/pages/blog/_id.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h2>Articulo # {{$route.params.id}}</h2>
+    <div class = "container mt-5 ">
+        <div class="card">
+            <div class="card-body">
+                <h1>{{articulo.title}}</h1>
+                <p>{{articulo.body}}</p>
+            </div>
+
+        </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return {
+            articulo:''
+        }
+    } , 
+    async created () {
+        const respuesta = await axios.get(`http://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+        this.articulo = respuesta.data;
+    }
+
+}
+</script>
--- nuxt/testapp/pages/blog/index.vue
+++ nuxt/testapp/pages/blog/index.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="container mt-5">
             <div class="row">
-                <div class="col-md-3" v-for="(item,index ) in articulos" :key="index">
-                    <h2><nuxt-link :to="`/blog/${item.id}`">{{item.titulo}}</nuxt-link></h2>
+                <div class="col-md-10" v-for="(item,index ) in articulos" :key="index">
+                       <div class = "container mt-5 ">
+                            <div class="card">
+                                <div class="card-header">
+                                <nuxt-link :to="`blog/${item.id}`">
+                                    <h1>{{item.title}}</h1>
+                                </nuxt-link>
+                                </div>
+                                <div class="card-body">
+                                    <p>{{item.body}}</p>
+                                </div>
+
+                            </div>
+                        </div>
 
                 </div>
             </div>
@@ -11,15 +23,32 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    head: {
+        title:"List from jsonplaceholder",
+        meta: {
+            articulos
+        }
+    },
     data(){
         return {
             articulos : [
-                {id:1 , titulo:"titulo 1"},
-                {id:2 , titulo:"titulo 2"},
-                {id:3 , titulo:"titulo 3"}
             ]
         }
+
+    },
+    async created(){
+        try {
+            const resp = await axios.get('http://jsonplaceholder.typicode.com/posts?_limit=10')
+            this.articulos = resp.data;
+            console.log(this.articulos)
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+
     }
 }
 </script>
